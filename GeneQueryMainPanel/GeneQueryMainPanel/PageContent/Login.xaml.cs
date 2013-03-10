a9726af5f52ae68634566bf22e7efd0080c5929d
Tim Yang <yzgogo2011@sina.com>
2013-03-10 23:01:29 +0800

    finish the login page

--- GeneQueryMainPanel/GeneQueryMainPanel/PageContent/Login.xaml.cs
+++ GeneQueryMainPanel/GeneQueryMainPanel/PageContent/Login.xaml.cs
@@ -19,9 +19,38 @@ namespace GeneQueryMainPanel.PageContent
     /// </summary>
     public partial class Window1 : Window
     {
+        private string userPass = "yangz2007";
+        MainPage mp = new MainPage();
+
         public Window1()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string input = this.passwordInput.Password;
+            if (input.Equals(userPass))
+            {
+                this.Content = mp.Content;
+            }
+            else
+            {
+                this.elertBox.Visibility = System.Windows.Visibility.Visible;
+                this.passwordInput.Password = "";
+            }
+        }
+
+        private void passwordInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            this.passwordInput.Password = "";
+            this.elertBox.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void login_cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.passwordInput.Password = "";
+            this.elertBox.Visibility = System.Windows.Visibility.Hidden;
         }
     }
 }
