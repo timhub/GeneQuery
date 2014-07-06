25bc0823740cd8994cc511d01902bcbc96cedad7
Tim Yang <yzgogo2011@sina.com>
2014-07-06 19:15:27 +0800

    NGUI and code refactor for 1.01a

---
+++ GeneQueryNGUI/GeneQueryNGUI/GeneQueryNGUI/DisplayFacadeBackingBean/BaseDataBindingBean.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace GeneQueryNGUI.DisplayFacadeBackingBean
+{
+    public class BaseDataBindingBean : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ GeneQueryNGUI/GeneQueryNGUI/GeneQueryNGUI/DisplayFacadeBackingBean/LoginPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneQueryNGUI.DisplayFacadeBackingBean
+{
+    public class LoginPageViewModel : BaseDataBindingBean
+    {
+        private String userName;
+        public String UserName 
+        {
+            get
+            {
+                return this.userName;
+            }
+            set
+            {
+                if (this.userName != value)
+                {
+                    this.userName = value;
+                    OnPropertyChanged("UserName");
+                }
+            }
+        }
+
+        private String userPass;
+        public String UserPass
+        {
+            get
+            {
+                return this.userPass;
+            }
+            set
+            {
+                if (this.userPass != value)
+                {
+                    this.userPass = value;
+                    OnPropertyChanged("UserPass");
+                }
+            }
+        }
+    }
+}
